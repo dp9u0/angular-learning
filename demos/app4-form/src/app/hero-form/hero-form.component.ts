d06ea8ea6182a4e93c5756581ecd53d220dc6027
guodp <guodp9u0@gmail.com>
2017-07-01 14:38:43 +0800

    code format

--- demos/app4-form/src/app/hero-form/hero-form.component.ts
+++ demos/app4-form/src/app/hero-form/hero-form.component.ts
@@ -14,11 +14,14 @@ export class HeroFormComponent implements OnInit {
   ngOnInit() {
   }
 
-  powers = ['Really Smart', 'Super Flexible',
-            'Super Hot', 'Weather Changer'];
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
   submitted = false;
+
   onSubmit() { this.submitted = true; }
+
   newHero() {
     this.model = new Hero(42, '', '');
   }
